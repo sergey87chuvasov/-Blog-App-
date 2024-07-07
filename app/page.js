0099e0280162200a572e502cc67752bc94786bcf
Sergey87 <serge.techno@gmail.com>
2024-07-07 23:42:44 +0300

    added Blog List for Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+'use client';
+
+import BlogList from '@/Components/BlogList';
 import Header from '@/Components/Header';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BlogList />
     </>
   );
 }
