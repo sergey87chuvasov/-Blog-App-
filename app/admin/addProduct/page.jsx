ca359cb3479462931fc3c2c113e46d6e3df656e9
Sergey87 <serge.techno@gmail.com>
2024-07-13 23:45:08 +0300

    Create Add Product Page - 2 part

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,14 +1,45 @@
 'use client';
 
 import { assets } from '@/Assets/assets';
+import axios from 'axios';
 import Image from 'next/image';
 import { useState } from 'react';
 
 const page = () => {
   const [image, setImage] = useState(false);
+
+  const [data, setData] = useState({
+    title: '',
+    description: '',
+    category: 'Startup',
+    author: 'Alex Bennet',
+    authorImg: '/author_img.png',
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+    console.log(data);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('category', data.category);
+    formData.append('author', data.author);
+    formData.append('authorImg', data.authorImg);
+    formData.append('image', image);
+
+    const response = await axios.post('/api/blog', formData);
+  };
+
   return (
     <>
-      <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
         <p className='text-xl'>Upload thumbnail</p>
         <label htmlFor='image'>
           <Image
@@ -28,6 +59,9 @@ const page = () => {
         />
         <p className='text-xl mt-4'>Blog title</p>
         <input
+          name='title'
+          onChange={onChangeHandler}
+          value={data.title}
           className='w-full sm:w-[500px] mt-4 px-4 py-3 border'
           type='text'
           placeholder='Type here'
@@ -35,6 +69,9 @@ const page = () => {
         />
         <p className='text-xl mt-4'>Blog description</p>
         <textarea
+          name='description'
+          onChange={onChangeHandler}
+          value={data.description}
           className='w-full sm:w-[500px] mt-4 px-4 py-3 border'
           type='text'
           placeholder='Write content here'
@@ -44,6 +81,8 @@ const page = () => {
         <p className='text-xl mt-4'>Blog category</p>
         <select
           name='category'
+          onChange={onChangeHandler}
+          value={data.category}
           className='w-40 mt-4 px-4 py-3 border text-gray-500'
         >
           <option value='Startup'>Startup</option>
