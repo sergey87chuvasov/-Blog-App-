f05995543a667ee02dd9cb7efa88c2381d5dc160
Sergey87 <serge.techno@gmail.com>
2024-07-15 00:35:29 +0300

    added Blog List Page for Admin - 1 part

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,20 @@
+import { assets } from '@/Assets/assets';
+import Image from 'next/image';
+
+const BlogTableItem = ({ authorImg, title }) => {
+  return (
+    <tr className='bg-white border-b'>
+      <th
+        scope='row'
+        className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'
+      >
+        <Image src={authorImg ? authorImg : assets.profile_icon} />
+      </th>
+      <td className='px-6 py-4'>{title ? title : 'no title'}</td>
+      <td className='px-6 py-4'>{'11 Jun 2024'}</td>
+      <td className='px-6 py-4'>x</td>
+    </tr>
+  );
+};
+
+export default BlogTableItem;
