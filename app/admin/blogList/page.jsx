db4e8ac4c34ca925951e9f74b73eb3e2ea54e3ec
Sergey87 <serge.techno@gmail.com>
2024-07-15 00:56:03 +0300

    finished Blog List Page for Admin

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,8 +1,21 @@
 'use client';
 
 import BlogTableItem from '@/Components/AdminComponents/BlogTableItem';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const page = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog');
+    setBlogs(response.data.blogs);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <div className='flex-1 pt-5 sm:pt-12 sm:pl-16'>
       <h1>All blogs</h1>
@@ -25,7 +38,18 @@ const page = () => {
             </tr>
           </thead>
           <tbody>
-            <BlogTableItem />
+            {blogs.map((item, index) => {
+              return (
+                <BlogTableItem
+                  key={index}
+                  mongoId={item._id}
+                  title={item.title}
+                  author={item.author}
+                  authorImg={item.authorImg}
+                  date={item.date}
+                />
+              );
+            })}
           </tbody>
         </table>
       </div>
